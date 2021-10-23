43a2b2205c8f7e058b5e898439bbc049566b419c
Joshua Potter <joshuaopotter@gmail.com>
2021-10-23 04:09:35 -0400

    Initial commit

---
+++ commands/nwqueue.js
@@ -0,0 +1,59 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const axios = require('axios').default;
+const { newWorldStatusToken } = require('../config.json');
+
+axios.defaults.baseURL = 'https://firstlight.newworldstatus.com/ext/v1';
+axios.defaults.headers.common['Authorization'] = `Bearer ${newWorldStatusToken}`;
+
+const getServer = (serverName, interaction) => {
+	axios.get(`/worlds/${serverName}`)
+		.then(async (response) => {
+			// handle success
+			// console.log(response.data);
+			const { message: { players_current, players_maximum, queue_current, queue_wait_time_minutes, status_enum } } = response.data;
+
+			// Server is down
+			if (!status_enum) {
+				await interaction.reply(`**${toTitleCase(serverName)}** is currently down.`);
+				return;
+			}
+
+			// Server has no queue
+			if (players_current < players_maximum) {
+				await interaction.reply(`There's no queue on **${toTitleCase(serverName)}**!`);
+				return;
+			}
+
+			// Server is full
+			// Display queue and wait time
+			if (players_current >= players_maximum) {
+				await interaction.reply(`The estimated queue length for **${toTitleCase(serverName)}** is **${queue_current}**. Estimated wait time is **${queue_wait_time_minutes} minutes**.`);
+			}
+		})
+		.catch(async (error) => {
+			// handle error
+			// console.log(error);
+			await interaction.reply({ content:`Could not retrieve server info for **${toTitleCase(serverName)}**, either it was an invalid server name or too many requests were sent in a short period of time. [Check your server name is correct](https://www.newworld.com/en-us/support/server-status) or try again later.`, ephemeral: true });
+		})
+		.then(() => {
+			// always executed
+		});
+};
+
+const toTitleCase = (string) => {
+	return string[0].toUpperCase() + string.slice(1).toLowerCase();
+};
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('nwqueue')
+		.setDescription('Replies with queue length and player count for a specific New World server.')
+		.addStringOption(option =>
+			option.setName('server')
+				.setDescription('The name of the New World server')
+				.setRequired(true)),
+	async execute(interaction) {
+		const serverName = interaction.options.getString('server');
+		getServer(serverName, interaction);
+	},
+};
---
+++ deploy-commands.js
@@ -0,0 +1,32 @@
+// This file registers commands for the bot.
+// You only need to run node deploy-commands.js once. You should only run it again if you add or edit existing commands.
+const fs = require('fs');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, token } = require('./config.json');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
