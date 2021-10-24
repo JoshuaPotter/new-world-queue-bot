0d0a520a9fc19865b78eeb58a9481d152abbb852
Joshua Potter <joshuaopotter@gmail.com>
2021-10-23 21:57:11 -0400

    Fix syntax

--- commands/nwqueue.js
+++ commands/nwqueue.js
@@ -18,19 +18,19 @@ const getServer = (serverName, interaction) => {
 				return;
 			}
 
-			await interaction.reply(`There are currently ${players_current}/${players_maximum} players connected to **${toTitleCase(serverName)**}.`);
+			const player_count = `There are currently ${players_current}/${players_maximum} players connected to **${toTitleCase(serverName)}**.`;
 
 			// Server has no queue
 			if (queue_current > 0) {
-				await interaction.reply(`The estimated queue length for **${toTitleCase(serverName)}** is **${queue_current}**. Estimated wait time is **${queue_wait_time_minutes} minutes**.`);
+				await interaction.reply(`${player_count} The estimated queue length is **${queue_current}**, with an estimated wait time of **${queue_wait_time_minutes} minutes**.`);
 			} else {
-				await interaction.reply(`There's no queue on **${toTitleCase(serverName)}**!`);
+				await interaction.reply(`${player_count} There is no queue!`);
 			}
 		})
 		.catch(async (error) => {
 			// handle error
 			// console.log(error);
-			await interaction.reply({ content:`Could not retrieve server info for **${toTitleCase(serverName)}**, either it was an invalid server name or too many requests were sent in a short period of time. [Check your server name is correct](https://www.newworld.com/en-us/support/server-status) or try again later.`, ephemeral: true });
+			await interaction.reply({ content:`Could not retrieve server info for **${toTitleCase(serverName)}**. [Check the server name was entered correctly](https://www.newworld.com/en-us/support/server-status) and try again.`, ephemeral: true });
 		})
 		.then(() => {
 			// always executed
